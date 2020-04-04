fb9632d069df10bfb92e0fa5c41dc6646e504107
Jakapong <jakapong.klainongsuang@gmail.com>
2020-04-04 15:19:31 +0700

    Set up redux

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import productReducer from './store/reducer/products';
+
+const rootReducer = combineReducers({
+  products: productReducer
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <View>...</View>
+    </Provider>
   );
 }
 
